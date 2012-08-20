7fe158fda09ca85c027939aba3fcce1d6ae805e6
Prosperent <support@prosperent.com>
2012-08-20 13:42:22 -0600

    Checking user module

--- public/javascripts/plink.js
+++ public/javascripts/plink.js
@@ -1,5 +1,6 @@
-CloudFlare.define("prosperlinks", ["prosperlinks/config", "cloudflare/config"], function(_config, _cfconfig)
+CloudFlare.define("prosperlinks", ["prosperlinks/config", "cloudflare/config", "cloudflare/user"], function(_config, _cfconfig, _user)
 {
     console.debug(_config);
     console.debug(_cfconfig);
+    console.debug(_user);
 });
